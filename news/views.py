35646c2b14d4d7eb6260a8a2ea2a1e81be7d4e8d
Wagner Lopes <wagnerlopesbr@hotmail.com>
2024-06-09 13:15:46 -0300

    WL - PROJECT: Spotnews - Req 5/10 v1

--- news/views.py
+++ news/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from news.models import News
 
 
@@ -8,3 +8,11 @@ def home(request):
         'news': news
     }
     return render(request, 'home.html', context)
+
+
+def news_details(request, id):
+    news = get_object_or_404(News, id=id)
+    context = {
+        'news': news
+    }
+    return render(request, 'news_details.html', context)
--- spotnews/urls.py
+++ spotnews/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from news import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("news.urls")),
+    path("", views.home, name="home-page"),
+    path("<int:id>/", views.news_details, name="news-details-page"),
 ]
 
 if settings.DEBUG:
